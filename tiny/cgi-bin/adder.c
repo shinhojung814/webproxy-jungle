0e4ffec98df164050d9c3891f63d8ffcb25105da
Shinho Jung <87798782+shinhojung814@users.noreply.github.com>
2021-09-24 11:23:37 +0900

    Update adder.c

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -5,9 +5,9 @@
 #include "csapp.h"
 
 int main(void) {
+    int num1 = 0, num2 = 0;
     char *buf, *p;
     char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
-    int n1 = 0, n2 = 0;
 
     /* Extract the two arguments */
     if ((buf = getenv("QUERY_STRING")) != NULL) {
@@ -17,8 +17,8 @@ int main(void) {
         strcpy(arg1, buf);
         strcpy(arg2, p + 1);
 
-        n1 = atoi(arg1);
-        n2 = atoi(arg2);
+        num1 = atoi(arg1);
+        num2 = atoi(arg2);
     }
 
     /* Make the response body */
@@ -26,11 +26,11 @@ int main(void) {
     sprintf(content, "Welcome to add.com: ");
     sprintf(content, "%sThe Internet addition portal.\r\n<p>", content);
     sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>",
-            content, n1, n2, n1 + n2);
-    sprintf(content, "%sThanks for visiting!\r\n, content");
+            content, num1, num2, num1 + num2);
+    sprintf(content, "%sThanks for visiting!\r\n", content);
 
     /* Generate the HTTP response */
-    printf("Connection: close \r\n");
+    printf("Connection: Close\r\n");
     printf("Content-length: %d\r\n", (int)strlen(content));
     printf("Content-type: text/html\r\n\r\n");
     printf("%s", content);
